8851f4d76a82ad671d17e6c9fd43dd0165ba491a
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-12 20:01:35 +1000

    Merged old Flask src with new backend

--- web/backend/server.py
+++ web/backend/server.py
@@ -2,6 +2,8 @@ import psycopg2
 import time
 import os
 from dotenv import dotenv_values
+from threading import Thread
+from flask import Flask
 
 from enviro.enviro_logging import get_data
 
@@ -12,21 +14,43 @@ LOOP_DELAY = 1  # seconds
 config = None
 conn = None
 cur = None
+running = True
+app = Flask(__name__)
 
 
-def process():
-    # Get data
-    enviro = get_data()
+@app.route("/")
+def index():
+    return "Hello, World!"
 
-    # Insert sensor data into database
-    cur.execute("INSERT INTO samples (temperature, pressure, humidity, light, oxidised, reduced, nh3) VALUES (%s, %s, %s, %s, %s, %s, %s)", enviro)
-    conn.commit()
+
+def read_all():
+    global conn
+    global cur
+    global running
+
+    next_time = time.time() + LOOP_DELAY
+    while running:
+        time.sleep(max(0, next_time - time.time()))
+
+        # Get data
+        enviro = get_data()
+
+        # Insert sensor data into database
+        cur.execute(
+            "INSERT INTO samples (temperature, pressure, humidity, light, oxidised, reduced, nh3) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            enviro,
+        )
+        conn.commit()
+
+        next_time += (time.time() - next_time) // LOOP_DELAY * LOOP_DELAY + LOOP_DELAY
 
 
 def main():
     global config
     global conn
     global cur
+    global running
+    global app
 
     # Load environment variables
     config = {
@@ -43,9 +67,13 @@ def main():
     )
     cur = conn.cursor()
 
-    # Loop forever
-    next_time = time.time() + LOOP_DELAY
-    while True:
-        time.sleep(max(0, next_time - time.time()))
-        process()
-        next_time += (time.time() - next_time) // LOOP_DELAY * LOOP_DELAY + LOOP_DELAY
+    # Start reading data
+    thread = Thread(target=read_all)
+    thread.start()
+
+    # Start web server
+    app.run(host=config["HOST"], debug=True)
+
+    # Cleanup
+    running = False
+    thread.join()
