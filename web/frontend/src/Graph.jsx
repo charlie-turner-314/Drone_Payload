b162d22dace0955243e5de8ae7a2bd4b17f52696
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-13 03:34:09 +1000

    Add basic enviro graph plotting

--- web/frontend/src/Graph.jsx
+++ web/frontend/src/Graph.jsx
@@ -8,10 +8,77 @@ export default function Graph() {
     const [data, setData] = useState(undefined)
 
     const options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
 
     async function getData() {
-        //
+        const response = await fetch('http://localhost:5000/data')
+        const json = await response.json()
+        if (json.error) {
+            console.log(json.data)
+            return
+        }
+
+        const id = json.data.map(d => d[0])
+        const temperature = json.data.map(d => d[1])
+        const pressure = json.data.map(d => d[2])
+        const humidity = json.data.map(d => d[3])
+        const light = json.data.map(d => d[4])
+        const oxidised = json.data.map(d => d[5])
+        const reduced = json.data.map(d => d[6])
+        const ammonia = json.data.map(d => d[7])
+
+        setData({
+            labels: id,
+            datasets: [
+                {
+                    label: "Temperature",
+                    data: temperature,
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    backgroundColor: "rgba(255, 99, 132, 0.2)"
+                },
+                {
+                    label: "Pressure",
+                    data: pressure,
+                    borderColor: "rgba(54, 162, 235, 1)",
+                    backgroundColor: "rgba(54, 162, 235, 0.2)"
+                },
+                {
+                    label: "Humidity",
+                    data: humidity,
+                    borderColor: "rgba(255, 206, 86, 1)",
+                    backgroundColor: "rgba(255, 206, 86, 0.2)"
+                },
+                {
+                    label: "Light",
+                    data: light,
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    backgroundColor: "rgba(75, 192, 192, 0.2)"
+                },
+                {
+                    label: "Oxidised",
+                    data: oxidised,
+                    borderColor: "rgba(153, 102, 255, 1)",
+                    backgroundColor: "rgba(153, 102, 255, 0.2)"
+                },
+                {
+                    label: "Reduced",
+                    data: reduced,
+                    borderColor: "rgba(255, 159, 64, 1)",
+                    backgroundColor: "rgba(255, 159, 64, 0.2)"
+                },
+                {
+                    label: "Ammonia",
+                    data: ammonia,
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    backgroundColor: "rgba(255, 99, 132, 0.2)"
+                }
+            ]
+        })
     }
 
     useEffect(() => {
@@ -22,7 +89,7 @@ export default function Graph() {
         <Card className="m-3">
             <CardHeader>Enviro Data</CardHeader>
             <CardBody>
-                <p>TODO</p>
+                {data ? <Line data={data} options={options} /> : null}
             </CardBody>
         </Card>
     )
