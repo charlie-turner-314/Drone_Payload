6fbf9609f8b2c150d6897624443d35cb5eeb8045
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-13 04:46:59 +1000

    Add polling for enviro graph

--- web/frontend/src/Graph.jsx
+++ web/frontend/src/Graph.jsx
@@ -7,6 +7,9 @@ import "chart.js/auto";
 export default function Graph() {
     const [data, setData] = useState(undefined)
 
+    const INTERVAL = 1000
+    const MAX_ENTRIES = 30
+
     const options = {
         scales: {
             y: {
@@ -15,22 +18,33 @@ export default function Graph() {
         }
     }
 
+    let start = 0
+    let results = []
+
     async function getData() {
-        const response = await fetch('http://localhost:5000/data')
+        const url = new URL('http://localhost:5000/data')
+        url.searchParams.append('start', start)
+
+        const response = await fetch(url)
         const json = await response.json()
         if (json.error) {
             console.log(json.data)
             return
         }
 
-        const id = json.data.map(d => d[0])
-        const temperature = json.data.map(d => d[1])
-        const pressure = json.data.map(d => d[2])
-        const humidity = json.data.map(d => d[3])
-        const light = json.data.map(d => d[4])
-        const oxidised = json.data.map(d => d[5])
-        const reduced = json.data.map(d => d[6])
-        const ammonia = json.data.map(d => d[7])
+        results = results.concat(json.data)
+        results = results.slice(-MAX_ENTRIES)
+
+        const id = results.map(d => d[0])
+        const temperature = results.map(d => d[1])
+        const pressure = results.map(d => d[2])
+        const humidity = results.map(d => d[3])
+        const light = results.map(d => d[4])
+        const oxidised = results.map(d => d[5])
+        const reduced = results.map(d => d[6])
+        const ammonia = results.map(d => d[7])
+
+        start = id[id.length - 1]
 
         setData({
             labels: id,
@@ -82,7 +96,10 @@ export default function Graph() {
     }
 
     useEffect(() => {
-        getData()
+        const interval = setInterval(() => {
+            getData()
+        }, INTERVAL)
+        return () => clearInterval(interval)
     }, [])
 
     return (
