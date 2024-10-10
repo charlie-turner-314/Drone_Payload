fe5c53190f861bb7bd6eea29fbe265685f23ac41
Jayden Grubb <jaydengrubb1@gmail.com>
2024-10-10 11:21:26 +1000

    Added crude TTS system

--- web/frontend/src/Imagery.jsx
+++ web/frontend/src/Imagery.jsx
@@ -1,6 +1,23 @@
 import { useEffect, useState } from "react";
 import { Card, CardBody, CardHeader } from "reactstrap";
 import { getServerURL } from "./common";
+import Speech from "speak-tts";
+
+const speech = new Speech()
+if (speech.hasBrowserSupport()) {
+    console.log("TTS supported")
+    speech.init().then((data) => {
+        console.log("TTS ready: ", data)
+        speech.speak({
+            text: 'Hello',
+            queue: false
+        }).catch(e => {
+            console.error("Error: ", e)
+        })
+    }).catch(e => {
+        console.error("Failed to init TTS: ", e)
+    })
+}
 
 export default function Imagery() {
     const [data, setData] = useState({
@@ -19,13 +36,22 @@ export default function Imagery() {
         const response = await fetch(url)
         const json = await response.json()
         if (json.error) {
-            console.log(json.data)
+            console.error(json.data)
             return
         }
 
         // FIXME: Sometimes data is null, IDK why
         if (!json.data)
             json.data = [null, null, null, null, null, null]
+
+        if (json.data[1] !== null) {
+            speech.speak({
+                text: `Valve state is ${json.data[1] ? "open" : "closed"}`,
+                queue: true
+            }).catch(e => {
+                console.error("Error: ", e)
+            })
+        }
 
         setData({
             valveState: json.data[1],
